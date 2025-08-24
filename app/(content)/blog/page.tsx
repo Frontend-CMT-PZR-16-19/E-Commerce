bacf20dba25b491ca7490dcb8700d8ad8b9260d8
bedirhan-1 <bedirhan9828@gmail.com>
2025-08-24 17:56:55 +0300

    fetch blog posts

--- app/(content)/blog/page.tsx
+++ app/(content)/blog/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import { title } from "@/components/primitives";
-import getAllPosts from "@/sanity/lib/postQueries";
 import { useEffect, useState } from "react";
 
 export default function BlogPage() {
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getPosts = async () => {
-      await fetch("http:/localhost:3000/api/post")
+      await fetch("/api/post")
         .then((response) => response.json())
-        .then((data) => setPosts(data));
+        .then((jsonResponse) => setPosts(jsonResponse.data));
     };
+
+    getPosts();
   }, []);
 
-  return (
-    <div>
-      <h1 className={title()}>Blog</h1>
-    </div>
-  );
+  if (posts.length === 0) {
+    return <div>yükleniyor...</div>;
+  }
+
+  return <div>{JSON.stringify(posts)}</div>;
 }
